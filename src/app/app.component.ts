b08f20c8d23d19df683ee7849ea500487b874a81
Amine <amine.saidi@esprit.tn>
2024-06-02 15:34:27 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {WelcomeService} from "./service/welcome.service";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'elearning-front';
+
+
+  constructor(private welcomeService : WelcomeService) {
+  }
+
+  ngOnInit() {
+    this.welcomeService.getMessage().subscribe(value => this.title = value.message);
+  }
 }
